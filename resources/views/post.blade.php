6ff046bd3cef93f0e5078a053f09f315e9d4578b
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-05-09 20:08:22 +0700

    fisrt design for post showing

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,15 @@
-@dd($post)
-
 @extends('layouts.main')
 
 @section('container')
+
+    <div class="container">
+        <img src="https://source.unsplash.com/500x500?{{ $post->category->name }}" alt="">
+        <h1>
+            {{ $post->title }}
+        </h1>
+        <p>
+            {{ $post->body }}
+        </p>
+    </div>
 
 @endsection
