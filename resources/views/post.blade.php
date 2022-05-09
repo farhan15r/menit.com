89ffc04a56be9af0024b6bd85f25b78889386740
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-05-09 22:20:44 +0700

    litte modification in post show page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,14 +2,16 @@
 
 @section('container')
 
-    <div class="container">
-        <img src="https://source.unsplash.com/500x500?{{ $post->category->name }}" alt="">
-        <h1>
-            {{ $post->title }}
-        </h1>
-        <p>
-            {{ $post->body }}
-        </p>
+    <div class="row mb-2">
+        <div class="col-md-8">
+            <img src="https://source.unsplash.com/500x500?{{ $post->category->name }}" class="img-fluid" alt="...">
+            <h1>
+                {{ $post->title }}
+            </h1>
+            <p>
+                {{ $post->body }}
+            </p>
+        </div>
     </div>
 
 @endsection
