859ce7b4a63fd05618b5376f1dc7295c0fdd736d
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-07-05 20:35:29 +0700

    delete btn in show page

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -11,7 +11,11 @@
 
                 <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
                 <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
-                <a href="" class="btn btn-danger"><span data-feather="trash-2"></span> Delete</a>
+                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger" type="submit" onclick="return confirm('Are you sure?')"><span data-feather="trash-2"></span> Delete</button>
+                </form>
 
                 <img src="https://source.unsplash.com/500x500?{{ $post->category->name }}" class="img-fluid my-2" alt="...">
                 <p>
