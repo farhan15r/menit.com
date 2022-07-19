ead4acab0616a9ef5b2da3df20dd5000e1973210
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-07-19 13:47:12 +0700

    delete solving

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -133,7 +133,7 @@ class DashboardPostController extends Controller
         if ($post->image) {
             Storage::delete($post->image);
         }
-        Post::destroy('id', $post->id);
+        Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post has been deleted');
     }
